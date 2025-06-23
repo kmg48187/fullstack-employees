b2ea64646c5d2adb01865cb65b1a52b8c951242b
Kristen George <kmstrumpfer@yahoo.com>
2025-06-23 15:34:13 -0400

    Finished

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE fullstack-employees (
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL,
+  birthday date NOT NULL,
+  salery integer NOT NULL
+);
 
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,12 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.name.text(),
+      birthsday: faker.date.past({ years: 50 }),
+      salary: faker.number.int({ min: 5000, max: 100000 }),
+    };
+    await createEmployee(employee);
+  }
 }
